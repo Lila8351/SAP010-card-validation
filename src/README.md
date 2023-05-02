37fc7801ad2976863b9d231be0e7acde5d75c31e
Lila <lilaar91@gmail.com>
2023-05-02 10:24:00 -0300

    Firsct commit

---
+++ src/README.md
@@ -0,0 +1,8 @@
+<h1 align="center"> Algoritmo de Luhn </h1>
+<br>
+<h2> Estrutura e funcionamento de uma página web simples com a funcionalidade de validar tarjetas de cartão, seguindo a lógica do <a href="https://google.github.io/comprehensive-rust/pt-BR/exercises/day-2/luhn.html">Exemploalgoritmo de Luhn.</a></h2>
+<br>
+<br>
+<p align="justify"> Projeto 01 - Laboratoria - turma 10. </p>
+<br>
+<p></p>
--- src/validator.js
+++ src/validator.js
@@ -1,49 +1,41 @@
-
-//const btn = document.querySelector("button");
-
-//btn.onclick = function() {
-//  const inputText = document.querySelector("inputText");
-//  window.alert.write(inputText);
-
-//}
-
-//if (14<=card.length<=16){
-
-// document.getElementById("clickButton").addEventListener("click", validateCardFunction());
-// let card = string;
-
-// function validateCardFunction(){
-//   card = document.getElementById("inputCard").value;
-//   console.log(typeof(card));
-//   console.log(card);
-//   console.log("oi");
-// }
 document.getElementById("startFunction").addEventListener("click", myFunction);
 
 function myFunction(){
   document.getElementById("replaceCard").value = document.getElementById("inputCard").value;
-  //const inputCardArray = [];
   const input = document.getElementById("inputCard").value;
-  const inputToChar = input.charAt();
-  //console.log(inputCardArray.length);
   const inputCardCharsArray = [];
+  const newDatasArray = [];
 
 
   for (let i=0; inputCardCharsArray.length < 16; i++){
     inputCardCharsArray.push(input.charAt(i));
+    if(i%2 !== 0){
+      const newData1 = inputCardCharsArray[i]*2;
 
-    //const inputCardChars = inputCardCharsArray.charAt(i).push;
-    //console.log(inputCardChars.length);
-    //console.log(inputCardChars);
+      if(newData1>=10){
+        const remainder = 1+ newData1%10;
+        newDatasArray.push(remainder);
+
+      }else{
+        newDatasArray.push(newData1);
+      }
+
+    } else {
+      const newData0 = inputCardCharsArray[i];
+      newDatasArray.push(newData0);
+    }
+    const numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    for (let j = 0; j < numberArray.length; j++) {
+      if (numberArray[j] != newDatasArray[i]){
+
+      }else{
+        
+      }
+
   }
-  //console.log(inputCardCharsArray);
-  let array2=[];
-  for (j=0; j<inputCardCharsArray.length; j++){
 
-
-  }
-
-
+  console.log(newDatasArray);
 
 }
 
