e30c7145cf889de67030cb4a1c3689a9238d8d74
Lila <lilaar91@gmail.com>
2023-05-05 12:12:10 -0300

    DOM Changes. Making tests avaiable.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 //import validator from './validator.js';
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("startFunction").addEventListener("click", myFunction);
+
+function myFunction(){
+  const input = document.getElementById("inputCard").value;
+  //document.getElementById("replaceCard").value = document.getElementById("inputCard").value;
+  //substituir esse valor pelo retorno da função validator.maskify(input);
+  //document.getElementById("replaceCard").value = validator.maskify(input);
+
+  const mask = validator.maskify(input);
+  document.getElementById("replaceCard").value = mask;
+  
+
+  console.log(validator);
+
+}
+
