1a83e348089ce378cc60058e59d2d0867b4bd8a6
Lila <lilaar91@gmail.com>
2023-05-08 11:11:04 -0300

    Inclui a função para identificar as bandeiras.

--- src/index.js
+++ src/index.js
@@ -19,16 +19,35 @@ function myFunction(){
   }
 
   const cardArray = [];
+  const firstNumber = [];
+  firstNumber.push(input.charAt(0));
 
   for (let i = 0; cardArray.length < 16; i++) {
     cardArray.push(input.charAt(i));
   }
   cardArray.reverse();
 
-  const isValid = validator.isValid(cardArray);
-  if (isValid === true) {
-    document.getElementById("trueOrFalse").value = "Cartão válido!";
-  }else{
-    document.getElementById("falseOrFalse").value = "Por favor, revise o número de seu cartão";
+  document.getElementById("chooseFlag").addEventListener("click", validarCartao);
+
+  function validarCartao() {
+    const isValid = validator.isValid(cardArray);
+
+    if (firstNumber == 4){
+      document.getElementById("flag").value = "visa";
+    } else if (firstNumber == 5){
+      document.getElementById("flag").value = "mastercard";
+    } else if (firstNumber == 6){
+      document.getElementById("flag").value = "elo";
+    } else if (firstNumber == 3){
+      document.getElementById("flag").value = "AMEX";
+    } else {
+      document.getElementById("flag").value = "unkwown"
+    }
+
+    if (isValid === true) {
+      document.getElementById("trueOrFalse").value = "É válido. Boas compras! ";
+    }else{
+      document.getElementById("trueOrFalse").value = "Não funcionou. Por favor, revise o número digitado e tente novamente.";
+    }
   }
 }
